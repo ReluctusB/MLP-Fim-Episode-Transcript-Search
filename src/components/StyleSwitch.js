dfaf13b29eaf30d205bfb9992959cd52ff7bb8e8
EndaHallahan <endah@udel.edu>
2021-07-01 14:37:29 -0400

    Localstorage for themes.

--- src/components/StyleSwitch.js
+++ src/components/StyleSwitch.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 class StyleSwitch extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			
-		}
 	}
 
 	switchTheme() {
@@ -20,6 +17,13 @@ class StyleSwitch extends Component {
 	    document.documentElement.setAttribute('data-theme', targetTheme)
 	    localStorage.setItem('theme', targetTheme);
 	}
+
+	componentDidMount() {
+		let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+		if (storedTheme) {
+			document.documentElement.setAttribute('data-theme', storedTheme)
+		}
+  	}
 
 	render() {
 		return (
